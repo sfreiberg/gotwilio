28f681150bca0878315addac108f84d01742ba02
Jake Januzelli <jake_boaconstrictor@yahoo.com>
2013-12-24 15:27:03 -0500

    restructured twiml.go, added more comments, and added the <Response> verb

--- twiml.go
+++ twiml.go
@@ -11,14 +11,7 @@ type Response struct {
 	Verbs []interface{}
 }
 
-// Message defines the <Message> Twiml verb
-type Message struct {
-	XMLName xml.Name `xml:"Message"`
-	Body    string   `xml:"Body"`
-	Media   string   `xml:"Media"`
-}
-
-// constructor to make it simpler
+// constructor method to make it simpler to create Responses
 func NewTwimlResponse() *Response {
 	return &Response{}
 }
@@ -29,11 +22,6 @@ func (resp *Response) addVerb(verb interface{}) {
 	resp.Verbs = newVerbs
 }
 
-// adds a message to the given response
-func (resp *Response) Message(body, media string) {
-	resp.addVerb(Message{Body: body, Media: media})
-}
-
 // makes a buffer, writes the standard xml header and beginning response tag
 // encodes all of the responses verbs as xml, and writes them to the buffer
 // closes the response, and writes the buffer's contents to the provided writer
@@ -41,8 +29,37 @@ func (resp *Response) SendTwimlResponse(w io.Writer) {
 	var b bytes.Buffer
 	b.WriteString(xml.Header)
 	b.WriteString("<Response>")
-	result, _ := xml.Marshal(resp.Verbs) // add error handling
+	result, _ := xml.Marshal(resp.Verbs) // TODO: add error handling
 	b.Write(result)
 	b.WriteString("</Response>")
 	w.Write(b.Bytes())
+}
+
+// these next structs define the XML encoding of Twiml verbs
+// e.g the Message struct defines the fields and attributes of the <Message> verb,
+// and the proper XML encoding
+
+type Message struct {
+	XMLName xml.Name `xml:"Message"`
+	Body    string   `xml:"Body,omitempty"`
+	Media   string   `xml:"Media,omitempty"`
+}
+
+type Redirect struct {
+	Url    string `xml:",chardata"`
+	Method string `xml:"method,attr"`
+}
+
+// these next methods handle adding the respective verbs to the given Response
+// e.g the Message method handles adding a Message verb to the given Response
+
+func (resp *Response) Message(body, media string) {
+	resp.addVerb(Message{Body: body, Media: media})
+}
+
+func (resp *Response) Redirect(url, method string) {
+	if method == "" {
+		method = "POST"
+	}
+	resp.addVerb(Redirect{url, method}) // TODO: require url
 }
