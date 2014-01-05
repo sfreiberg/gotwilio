3c1860dc1d06e7815b0d368a8aff88f34868b179
Jake Januzelli <jake_boaconstrictor@yahoo.com>
2014-01-05 10:36:43 -0500

    cleaned up tests

--- gotwilio_test.go
+++ gotwilio_test.go
@@ -41,11 +41,7 @@ func TestVoice(t *testing.T) {
 	}
 }
 
-func TestValidate(t *testing.T) {
-
-}
-
-func TestMessage(t *testing.T) {
+func ExampleMessage(t *testing.T) {
 	resp := NewTwimlResponse()
 	resp.Message(Message{Body: "test", To: "+16169165421"})
 	resp.SendTwimlResponse(os.Stdout)
