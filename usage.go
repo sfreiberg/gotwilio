57400bbefd7106fc82eb2b08b8858be69bff1556
Jake Januzelli <januzellij@gmail.com>
2014-04-03 17:44:00 -0400

    Added UsageRecord

--- usage.go
+++ usage.go
@@ -2,3 +2,18 @@ package gotwilio
 
 // https://twilio-python.readthedocs.org/en/latest/usage/usage.html
 // and https://www.twilio.com/docs/api/rest/usage-records
+
+type UsageRecord struct {
+	Category    string
+	Description string
+	AccountSid  string
+	StartDate   string
+	EndDate     string
+	Usage       string
+	UsageUnit   string
+	Count       int64
+	CountUnit   string
+	Price       float32
+	PriceUnit   string
+	Url         string
+}
