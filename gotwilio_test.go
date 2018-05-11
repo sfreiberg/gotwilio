e6215cfb56793e1f921273345ee3f7ae94f1cba5
Joe Heth <joeheth@gmail.com>
2018-05-11 17:49:44 -0400

    Add proxy participant and message interaction support

--- gotwilio_test.go
+++ gotwilio_test.go
@@ -1,10 +1,13 @@
 package gotwilio
 
 import (
+	"fmt"
 	"testing"
 )
 
 var params map[string]string
+var ServiceSid = ""
+var SessionSid = ""
 
 func init() {
 	params = make(map[string]string)
@@ -171,5 +174,113 @@ func TestProxySessionCRUD(t *testing.T) {
 	if exc != nil {
 		t.Fatal(exc)
 	}
+
+}
+
+func TestParticipantCreationAndMessage(t *testing.T) {
+	t.Skip("Skipping as default test")
+
+	twilio := NewTwilioClient(params["SID"], params["TOKEN"])
+
+	service, exc, err := twilio.GetProxyService(ServiceSid)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if exc != nil {
+		t.Fatal(exc)
+	}
+
+	// Create ProxySession
+	// req := ProxySessionRequest{
+	// 	UniqueName: "Session#1234",
+	// 	Mode:       "message-only",
+	// }
+
+	// session, exc, err := twilio.NewProxySession(service.Sid, req)
+	session, exc, err := twilio.GetProxySession(service.Sid, SessionSid)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if exc != nil {
+		t.Fatal(exc)
+	}
+
+	// List Participants
+	participants, exc, err := session.ListParticipants()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if exc != nil {
+		t.Fatal(exc)
+	}
+	fmt.Println("Participant Count", len(participants))
+	fmt.Println("")
+
+	// Add Participant
+	var participant Participant
+	if len(participants) == 0 {
+		u := ParticipantRequest{
+			FriendlyName: "John Doe",
+			Identifier:   "+15715551234",
+		}
+
+		participant, exc, err = session.AddParticipant(u)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if exc != nil {
+			t.Fatal(exc)
+		}
+	} else {
+		// Get first one
+		participant, exc, err = session.GetParticipant(participants[0].Sid)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if exc != nil {
+			t.Fatal(exc)
+		}
+		fmt.Printf("Participant: %v\n\n", participant)
+	}
+
+	msg := ProxyMessage{
+		Body: "A follow up message",
+	}
+	resp, exc, err := session.CreateInteraction(participant.Sid, msg)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if exc != nil {
+		t.Fatal(exc)
+	}
+
+	fmt.Printf("Message Interaction: %v\n\n", resp)
+
+	interactions, exc, err := session.GetInteractions()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if exc != nil {
+		t.Fatal(exc)
+	}
+	fmt.Println("Interaction Count", len(interactions.Interactions))
+
+	// participant, exc, err := session.DeleteParticipant(participant.ID)
+	// if err != nil {
+	// 	t.Fatal(err)
+	// }
+	// if exc != nil {
+	// 	t.Fatal(exc)
+	// }
+
+	// // Delete Session
+	// exc, err = twilio.DeleteProxySession(service.Sid, session.Sid)
+	// if err != nil {
+	// 	t.Fatal(err)
+	// }
+
+	// if exc != nil {
+	// 	t.Fatal(exc)
+	// }
 
 }
